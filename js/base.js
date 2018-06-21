3786c0354f7177214f9fb5f659f894261c2be080
qiubite <meqiuayng@gmail.com>
2018-06-22 00:19:45 +0800

    笔记

---
+++ js/base.js
@@ -0,0 +1,13 @@
+$(function () {
+    // 添加导航条
+    $("#nav-placeholder").load("nav.html");
+});
+
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?a3debbbc2a7afabd92a717550f888c88";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+
---
+++ js/mdAndHtml.js
@@ -0,0 +1,7 @@
+$(function () {
+    //添加文章导航侧边栏
+    $(".left-div").prepend($(".md-toc"));
+    //文章主题conet
+    $(".right-div").prepend($("#write"));
+});
+
